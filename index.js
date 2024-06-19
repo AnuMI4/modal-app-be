a4a5e987bb657896f3d2c271610392b6d7f33703
Anum Mirza <anoom.16@gmail.com>
2024-06-19 15:45:29 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,74 @@
+// index.js
+
+// Load environment variables from .env file (if using dotenv)
+require('dotenv').config();
+
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+const awsServerlessExpress = require('aws-serverless-express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware to parse JSON request body
+app.use(bodyParser.json());
+
+// Middleware to add CORS headers to every response
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Adjust the '*' to your specific domain if needed
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  // Handle preflight requests
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello hi');
+});
+
+// POST endpoint to handle sending emails
+app.post('/send-email', (req, res) => {
+  const { contactInfo } = req.body;
+
+  let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_USER, // Using environment variable
+      pass: process.env.EMAIL_PASS // Using environment variable
+    }
+  });
+
+  let mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: process.env.EMAIL_USER,
+    subject: 'New Contact Information',
+    text: `Contact Info: ${contactInfo}`
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error('Error sending email:', error);
+      res.status(500).send('Failed to send email');
+    } else {
+      console.log('Email sent:', info.response);
+      res.status(200).send('Email sent successfully');
+    }
+  });
+});
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+const server = awsServerlessExpress.createServer(app);
+
+exports.handler = (event, context) => {
+  awsServerlessExpress.proxy(server, event, context);
+};
